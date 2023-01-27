98c8ae4f546020f1ae2279ea59046646d953c846
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2023-01-27 20:35:38 +0100

    init

---
+++ FoodTracker/mainwindow.cpp
@@ -0,0 +1,84 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QDate>
+#include <QDateTime>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_lineEdit_add_item_returnPressed()
+{
+    // Get the current number of rows in the tableWidget
+    int row = ui->tableWidget->rowCount();
+
+    // Insert a new row at the end of the tableWidget
+    ui->tableWidget->insertRow(row);
+
+    // Create a new QTableWidgetItem with the text "test"
+    QTableWidgetItem* newItem = new QTableWidgetItem(ui->lineEdit_add_item->text());
+
+    // Insert the new QTableWidgetItem into the new row in the first column
+    ui->tableWidget->setItem(row, 0, newItem);
+    ui->lineEdit_add_item->clear();
+}
+
+
+void MainWindow::on_pushButton_add_item_clicked()
+{
+    // Get the current number of rows in the tableWidget
+    int row = ui->tableWidget->rowCount();
+
+    // Insert a new row at the end of the tableWidget
+    ui->tableWidget->insertRow(row);
+
+    // Create a new QTableWidgetItem with the text "test"
+    QTableWidgetItem* newItem = new QTableWidgetItem(ui->lineEdit_add_item->text());
+
+    // Insert the new QTableWidgetItem into the new row in the first column
+    ui->tableWidget->setItem(row, 0, newItem);
+    ui->lineEdit_add_item->clear();
+}
+
+
+void MainWindow::on_tableWidget_cellChanged(int row, int column)
+{
+    if(column == 1){
+        QString dateString;
+        QTableWidgetItem* item = ui->tableWidget->item(row, column);
+        if (item != nullptr) {
+            dateString = item->text();
+        } else {
+            // handle empty cell
+            return;
+        }
+
+        QDate date = QDate::fromString(dateString, "dd-MM-yyyy");
+        if (!date.isValid()) {
+            // Handle invalid date input
+            return;
+        }
+
+        QDate today = QDate::currentDate();
+        int daysUntil = date.daysTo(today) * -1;
+        QString daysUntilS = QString::number(daysUntil);
+
+        QTableWidgetItem* newItem = new QTableWidgetItem(daysUntilS);
+
+        // Insert the new QTableWidgetItem into the new row in the next column
+        ui->tableWidget->setItem(row, 2, newItem);
+    }
+}
+
+
---
+++ FoodTracker/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_lineEdit_add_item_returnPressed();
+
+    void on_pushButton_add_item_clicked();
+
+    void on_tableWidget_cellChanged(int row, int column);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
